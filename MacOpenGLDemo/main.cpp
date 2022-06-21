6ff0c1e34f0e145fabbe4ee8981ba5ca93e1b281
liuke <liuke.rd@bytedance.com>
2022-06-21 18:17:30 +0800

    完成第一个窗口渲染

--- MacOpenGLDemo/main.cpp
+++ MacOpenGLDemo/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     if (!glfwInit()) {
         std::cout << "glfw init failed" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "播放器", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
